90c652119361203bc14aa1eebd7e12cc93e420a5
YangJiaqi <yang.jiaqi.hadoop@gmail.com>
2023-06-29 15:51:33 +0800

    code LinkList by compilot

---
+++ src/main/java/com/graph/basicalgostructure/LInkList.java
@@ -0,0 +1,42 @@
+package com.graph.basicalgostructure;
+
+public class LInkList {
+    //你是一名程序员，请用java 实现双向链表遍历。
+    public static void main(String[] args) {
+        LInkList lInkList = new LInkList();
+        Node head = lInkList.createLinkList();
+        lInkList.traverseLinkList(head);
+    }
+
+    //创建双向链表
+    public Node createLinkList() {
+        Node head = new Node(-1);
+        Node p = head;
+        for (int i = 0; i < 10; i++) {
+            Node node = new Node(i);
+            p.next = node;
+            node.prev = p;
+            p = node;
+        }
+        return head;
+    }
+
+    //遍历双向链表
+    public void traverseLinkList(Node head) {
+        Node p = head.next;
+        while (p != null) {
+            System.out.println(p.data);
+            p = p.next;
+        }
+    }
+    //请创建Node 类
+    class Node {
+        private int data;
+        private Node prev;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+}
